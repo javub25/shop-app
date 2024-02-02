4f3048360f4f579ce6badca174194a8b580925f7
javub25 <ju231415sad@gmail.com>
2024-02-02 12:39:19 +0100

    feat(Login.jsx): add Login component to handle user login and registration functionality
    The Login component is responsible for handling user login and registration functionality. It imports the Firebase module for authentication, React for state management, and the UserContext for accessing user data. It also imports other necessary components such as Input, handleErrorMsg, and SendData.
    
    The component defines a function called Registering, which toggles the Register state. The Register state determines whether the user is registering or logging in.
    
    The main JSX code renders a form with email and password inputs. The form's onSubmit event is handled by the SendData function, which takes care of sending the user's data to the server. The handleErrorMsg function is used to handle any error messages that may occur during the form submission. The UserContext and Register state are also passed as parameters to the SendData function.
    
    The JSX code also includes a span element to display any error messages, and a button for submitting the form. The button's text and functionality depend on the Register state.
    
    Finally, there is a button for toggling the Register state, which changes the text and functionality of the main button.
    
    This component is a new addition to the project and is not yet used elsewhere.

---
+++ src/features/Login/Login.jsx
@@ -0,0 +1,48 @@
+import Firebase from "../Firebase/Firebase.jsx";
+import React from "react";
+import UserContext from "../Context/UserContext.jsx";
+import {Input} from "@nextui-org/input";
+import handleErrorMsg from "../Form/handleErrorMsg.jsx";
+import SendData from "../Form/SendData.jsx"
+
+const Login = () => 
+{
+    const app = Firebase();
+    
+    const setUsuario = React.useContext(UserContext);
+
+    const [Register, setRegister] = React.useState(false);
+    const [MessageError, setMessageError] = React.useState("");
+
+    const Registering = () => 
+    {
+        setRegister(oldValue => !oldValue);
+    }
+    
+    return (
+            <main className="mt-10 mb-5 mx-auto w-[400px] mobile:w-full bg-white border-[#ffa8af] p-16 border-solid border-2 rounded-md">
+                <h1 className="text-[40px] text-[#ff6f7b] mb-8">{Register ? "Crea una cuenta": "Iniciar sesión"}</h1>
+                <form onSubmit={(e) => SendData(
+                    {
+                        event: e,
+                        eventMsgError: handleErrorMsg,
+                        changeMsgError: setMessageError,
+                        changeUser: setUsuario,
+                        registerState: Register,
+                    })}>
+                        
+                    <Input type="text" id="email" variant={"underlined"} label="Email" 
+                       classNames={{inputWrapper: "border-[#ffa8af]"}}       
+                    />
+                     <Input type="password" id="password" variant={"underlined"} label="Password" 
+                        classNames={
+                                    {inputWrapper: "border-[#ffa8af]", base: "pb-[14px]"}}         
+                    />
+                    <span className="text-sm text-red-400 py-[14px]">{MessageError}</span>
+                    <br/><button className="bg-[#ff8f98] mt-12 my-6 hover:scale-110 hover:border-0 text-balance" type="submit">{Register ? "Crea una cuenta de forma muy rápida": "Iniciar sesión"}</button>
+                </form>
+                <button className="bg-[#ff8f98] hover:scale-110 hover:border-0 text-balance" onClick={() => Registering()}>{Register ? "¿Ya tienes cuenta? inicia sesión": "¿No tienes cuenta? Únete a nosotros"}</button> 
+            </main>
+    )
+}
+export default Login;
