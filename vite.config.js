706ec7a3c1b88e9823d1151df85859387a1c1174
javub25 <ju231415sad@gmail.com>
2024-02-07 13:41:40 +0100

    chore(vite.config.js): add aliases for commonly used directories to improve module resolution
    The aliases '@api', '@features', and '@assets' are added to the resolve configuration in vite.config.js. These aliases are set to their respective directory paths to simplify module imports and improve code readability.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import million from "million/compiler";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [million.vite({ auto: true }), react()],
+
+  resolve: {
+    alias: [
+      {
+        find: '@api',
+        replacement: path.resolve(path.join(__dirname, '/src/api')),
+      },
+      {
+        find: '@features',
+        replacement: path.resolve(path.join(__dirname, '/src/features')),
+      },
+      {
+        find: '@assets',
+        replacement: path.resolve(path.join(__dirname, '/src/assets')),
+      },
+    ]
+  }
 })
