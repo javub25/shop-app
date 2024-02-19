d2941b7f532590efee15c90e29fc459dd937da8c
javub25 <ju231415sad@gmail.com>
2024-02-19 18:43:40 +0100

    fix(Products.jsx): fix import paths for getProducts and Pagination components feat(Products.jsx): add header with title "Products" and styling feat(Products.jsx): generate 10 unique UUIDs for each product page feat(Products.jsx): add link to product details page for each product feat(Products.jsx): improve styling and layout of product grid feat(Products.jsx): add conditional rendering for when there are no products on the page

--- src/features/Products/Products.jsx
+++ src/features/Products/Products.jsx
@@ -1,47 +1,52 @@
-import getProducts from "@api/getProducts";
-import Pagination from "@features/Pagination/ModulPage.jsx"
+import getProducts from "@api/getProducts.jsx";
+import Pagination from "@features/Pagination/ModulPage.jsx";
 import React from "react";
+import {Link} from "wouter";
 
 const Products = () => 
 {
     const products = getProducts();
     const [page, setPage] = React.useState(0);
+    //Let's generate 10 uniqueUUID for each product page
+    const uniqueUUID = Array.from({ length: 10 }, () => self.crypto.randomUUID());
 
+    
     return (
         <main>
+            <header className="py-12 px-12 mobile:px-0">
+                <h1 className="text-[#ff6f7b] text-left mobile:text-center text-[23px] font-bold">Products</h1>
+            </header>
             {
                 products.length === 0 ?
-                    <div className="lds-circle py-[50px]"><div></div></div>
+                    <p className="text-black text-left pb-12 px-12 mobile:px-0">There aren't products in this page.</p>
                 :
                 <>
-                    <header className="py-12 px-12 mobile:px-0">
-                        <h1 className="text-[#ff6f7b] text-left mobile:text-center text-[23px] font-bold">Productos</h1>
-                    </header>
-
-                    <section className="grid grid-cols-2 gap-11 tablet:grid-cols-1">
+                    <section className="grid grid-cols-3 tablet:grid-cols-2 gap-[2rem] mobile:grid-cols-1">
                     {
                         /* The code `products[page].map((product => {...}))` is iterating over the
                         array of products at the current page index (`products[page]`). It is using
                         the `map` function to create a new array of JSX elements based on each
                         product in the `products[page]` array. */
-                        products[page].map((product => 
+                        products[page].map((product, i) => 
                         {
                             return (
-                                <>
-                                <article key={product.id} className="place-items-center items-center grid grid-cols-2 mobile:grid-cols-1">
-                                    <div>
-                                        <img src={product.image} className="w-[120px] mobile:mb-8" alt={product.title}/>
-                                    </div>
-                                    
-                                    <div>
+                                <article key={uniqueUUID[i]}>
+                                        {/*In this case, it is creating a link
+                                                to the "/product" route. When the link is clicked, it
+                                                will navigate to the "/product" page and pass the
+                                                `productData` object as state */
+                                        }
+                                        <Link href="/product" state={{ productData: product }}>
+                                            <img width="160" src={product.image} className="mb-8 mx-auto object-contain" alt={product.title}
+                                                style={{height:"205px"}} />
+                                        </Link>
+
                                         <span className="bg-black py-2 px-4 text-white text-sm">{product.category}</span><br/><br/>
                                         <h2 className="pb-2 font-bold text-base text-balance text-black">{product.title}</h2>
                                         <span className="text-[30px] text-[#ff6f7b] font-bold">{product.price} € </span>
-                                    </div>    
                                 </article>
-                            </>
                             )
-                        }))
+                        })
                     }
                     </section>
 
@@ -54,7 +59,6 @@ const Products = () =>
                     </footer>    
                 </>
             }
-            
         </main>
     )
 }
