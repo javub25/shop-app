a05fe418698f8db16fe1f17761965b047b1a3373
javub25 <ju231415sad@gmail.com>
2024-02-19 18:39:13 +0100

    fix(App.jsx): fix import order to improve readability and maintain consistency feat(App.jsx): add support for displaying a cart modal using the useDisclosure hook from NextUI library

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import React, {Suspense, lazy} from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {Route} from "wouter";
+import {NextUIProvider} from "@nextui-org/system";
+import { useDisclosure } from "@nextui-org/react";
+
 import Firebase from "@features/Firebase/Firebase.jsx";
 import UserContext from "@features/Context/UserContext.jsx"
-import {NextUIProvider} from "@nextui-org/system";
 import Header from "@features/Header/Header.jsx"
-import {Route} from "wouter";
+import Cart from "@features/Cart/Cart.jsx";
 
 /* The code is using the `lazy` function from React to dynamically import the components `Products`,
 `Product`, `Login`, and `Home`. */
@@ -14,11 +17,15 @@ const Product = lazy(() => import("@features/Products/SingleProduct.jsx"));
 const Login = lazy(() => import("@features/Login/Login.jsx"));
 const Home = lazy(() => import("@features/Home/Home.jsx"));
 
-
 function App() {
   const app = Firebase();
   const [usuario, setUsuario] = React.useState(null)
   const auth = getAuth();
+
+/* The line `const { isOpen, onClose, onOpen } = useDisclosure();` is using the `useDisclosure` hook
+from the NextUI library to manage the state of a modal in the parent component. */
+  const { isOpen, onClose, onOpen } = useDisclosure(); 
+
   
   /* The `React.useEffect` hook is used to perform side effects in a functional component. In this
   case, it is used to listen for changes in the authentication state using the `onAuthStateChanged`
@@ -37,26 +44,30 @@ function App() {
   }, [])
 
     return (
-    <>
-    <Header/>
-      <NextUIProvider>
-        {/*The `Suspense it is rendering a loading
-        spinner with the class name "lds-circle". 
-        This spinner will be displayed until the lazy-loaded components (`Products`, `Product`, `Categories`, `Login`, and `Home`)
-        are fully loaded and ready to be rendered.*/}
-        
-        <Suspense fallback={<div className="lds-circle py-[50px]"><div></div></div>}>
-          <Route path="/products" component={Products}></Route>
-          <Route path="/product" component={Product}></Route>
-            <UserContext.Provider value={
-              [usuario, setUsuario]
-            }>
-            {usuario ? <Home /> : <Route path="/login" component={Login}></Route>
-            }
-            </UserContext.Provider>
-        </Suspense>
-      </NextUIProvider>
-    </>
+      <>
+      <Header onOpen={onOpen}/>
+        <NextUIProvider>
+          {/*The `Suspense it is rendering a loading
+          spinner with the class name "lds-circle". 
+          This spinner will be displayed until the lazy-loaded components (`Products`, `Product`, `Categories`, `Login`, and `Home`)
+          are fully loaded and ready to be rendered.*/}
+          
+          <Suspense fallback={<div className="lds-circle py-[50px]"><div></div></div>}>
+              <Route path="/products" component={Products}></Route>
+              <Route path="/product" component={Product}></Route>
+                
+                <UserContext.Provider value={
+                  [usuario, setUsuario]
+                }>
+                {usuario ? <Home /> : <Route path="/login" component={Login}></Route>
+                }
+                </UserContext.Provider>
+          </Suspense>
+
+          <Cart isOpen={isOpen} onClose={onClose} />
+
+        </NextUIProvider>
+      </>
   )
 }
 
