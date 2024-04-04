f391f797d04474fe3f604eed0407ddb2668f1398
javub25 <ju231415sad@gmail.com>
2024-04-04 20:50:01 +0200

    Adding every cart actions

---
+++ src/features/Context/CartContext.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+/*1) We're gonna create a cart context to sincronize changes products in our localStorage between 
+SingleProduct.jsx and Cart.jsx*/
+const CartContext = React.createContext();
+
+/*2) We create a provider that will receives SingleProducts.jsx and Cart.jsx as children.
+They will get access to latest cart data every time using cart variable globally*/ 
+const CartProvider = ({children}) => 
+{
+    const [cart, setCart] = React.useState(JSON.parse(localStorage.getItem("productsCart")) || [])
+    const newCart = [...cart];
+
+    const addProduct = (product, cart, setCart) => {
+        //We're looking for the current index of the existing product
+        const index = cart.findIndex((productsCart => productsCart.id === product.id))
+        
+        //If product already exists we need to increase quantity and get the new price of product
+        if(index!==-1) 
+        {
+            newCart[index].quantity+=1;
+            newCart[index].totalPrice = newCart[index].basePrice * newCart[index].quantity;
+        }
+        //Otherwise we pass the initial product
+        else {
+            newCart.push({
+                id: product.id,
+                image: product.image,
+                name: product.title,
+                quantity: 1, 
+                basePrice: product.price,
+                totalPrice: product.price,
+            }
+            )
+        }
+        setCart(newCart)
+    }
+
+    /*it returns every object doesn't contain id parameter*/
+    const removeProduct = (setCart, id) => setCart((oldProduct) => oldProduct.filter(item => item.id!==id))
+
+    /*The `removeCart` function clears the contents of the cart by setting it to an empty array.*/
+    const removeCart = (setCart) => setCart([])
+
+    /*The removeQuantityCart will decrease quantity for each product*/
+    const removeQuantityCart = (index, setCart) =>{
+        newCart[index].quantity = newCart[index].quantity <= 1 ? 1 : newCart[index].quantity - 1;
+        newCart[index].totalPrice = newCart[index].quantity <=1 ? newCart[index].basePrice : newCart[index].totalPrice - newCart[index].basePrice;
+        setCart(newCart);
+    }
+
+    return (
+        <CartContext.Provider value={
+            [cart, 
+            setCart, 
+            addProduct, 
+            removeProduct, 
+            removeCart,
+            removeQuantityCart
+            ]}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+export {CartContext, CartProvider};
